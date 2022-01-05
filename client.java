3e9f75b44779d8b7340d28ad020d302c4fd8c734
Zephyr <constantinmihail13@yahoo.com>
2022-01-05 23:18:30 +0200

    Optimised code, but the server won't stop running

--- client.java
+++ client.java
@@ -1,16 +1,35 @@
 import java.net.*;
 import java.io.*;
 
+public class test_DHCP_Client {
+    public static void main(String[] args) throws IOException{
+        client s = new client();
+        s.sendMessage("Mama");
+        s.sendMessage("are");
+        s.sendMessage("mere");
+        s.sendMessage("end");
+        s.close();
+    }
+}
+
 public class client{
     final static int SERVER_PORT_NUMBER=67;
     final static int CLIENT_PORT_NUMBER=68;
-    public static void main(String[] args) throws IOException{
-        DatagramSocket client = new DatagramSocket();
-        InetAddress address = InetAddress.getByName("localhost");
-        String string = "Success!";
-        byte[] buffer = string.getBytes();
+    private static InetAddress address;
+    private static  DatagramSocket client;
+    private static byte[] buffer;
+
+    public client() throws IOException{
+        client = new DatagramSocket();
+        address = InetAddress.getByName("localhost");
+    }
+    public void sendMessage(String message)throws IOException{
+        buffer = message.getBytes();
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length,address,SERVER_PORT_NUMBER);
         client.send(packet);
+        
+    }
+    public void close(){
         client.close();
     }
 }
--- server.java
+++ server.java
@@ -1,17 +1,37 @@
 import java.net.*;
 import java.io.*;
 
+public class test_DHCP_Server {
+    public static void main(String[] args) throws IOException{
+        server s = new server();
+        s.recieveMessage();
+    }
+}
+
 public class server {
     final static int SERVER_PORT_NUMBER=67;
     final static int CLIENT_PORT_NUMBER=68;
-    public static void main(String[] args) throws IOException{
-        DatagramSocket server = new DatagramSocket(SERVER_PORT_NUMBER);
-        byte[] buffer = new byte[256];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        server.receive(packet);
-        String response = new String(packet.getData());
-        System.out.println("Response data : "+response);
+    private static boolean running;
+    private static  DatagramSocket server;
+    private static byte[] buffer;
+
+    public server() throws IOException{
+        server = new DatagramSocket(SERVER_PORT_NUMBER);
+    }
+    public void recieveMessage()throws IOException{
+        running = true;
+        while(running){
+            buffer = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            server.receive(packet);
+            String response = new String(packet.getData());
+            System.out.println("Response data : "+response);
+            if (response.equals("end")) {
+                running = false;
+                continue;
+            }
+        }
         server.close();
     }
-    
 }
+
