27512b238f5fad18e4e4c8bd3bc40e511963cac9
Zephyr <constantinmihail13@yahoo.com>
2022-01-15 23:07:09 +0200

    UDP server-client communication

--- client.py
+++ client.py
@@ -1,8 +1,12 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 5000))
-s.sendall(bytes('Hello, worlda', encoding="ascii"))
-data = s.recv(1024)
-print('Am receptionat: ', data)
-s.close()
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udp_host = socket.gethostname()		
+udp_port = 5000		
+udp_address = (udp_host,udp_port)	        
+
+msg = b"Hello Python!"
+print ("UDP target IP:",udp_host)
+print ("UDP target Port:",udp_port)
+
+s.sendto(msg,udp_address)
--- server.py
+++ server.py
@@ -1,15 +1,11 @@
 import socket
+from xml.etree.ElementTree import tostring
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind(('127.0.0.1', 5000)) 
-s.listen(1)
-print('Asteapta conexiuni') 
-conn, addr = s.accept()
-print('S-a conectat clientul', addr, ' la serverul ', conn.getsockname())
-while 1:
-    data = conn.recv(1024) 
-    if not data:
-     break
-    print('Am receptionat: ', data)
-    conn.sendall(bytes('Echo: ' + str(data), encoding="ascii"))
-conn.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+udp_host = socket.gethostname()		
+udp_port = 5000	
+s.bind((udp_host, udp_port)) 
+while True:
+    print ("Se asteapta un client...")
+    data,addr = s.recvfrom(1024)
+    print("Mesaj: ",data," de la ",addr)
